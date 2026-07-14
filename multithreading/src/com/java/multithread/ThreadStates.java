f10ef50e0eb9d73eb2ee65d98daab0cbe0c890c9
nikita <nikitabajaj790@gmail.com>
2026-07-14 17:47:35 +0530

    Different States of Thread

--- multithreading/src/com/java/multithread/ThreadStates.java
+++ multithreading/src/com/java/multithread/ThreadStates.java
@@ -1,4 +1,28 @@
 package com.java.multithread;
 
-public class ThreadStates {
+public class ThreadStates extends Thread{
+
+    @java.lang.Override
+    public void run() {
+        System.out.println("RUNNING");
+        try{
+            Thread.sleep(2000); //thread t1 sleeps
+        }catch (InterruptedException e){
+            System.out.println(e);
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ThreadStates t1 = new ThreadStates();
+        System.out.println(t1.getState());
+        t1.start();
+        System.out.println(t1.getState());
+        System.out.println(Thread.currentThread().getState()); //main thread state
+        Thread.sleep(100); //pause main thread
+        System.out.println(t1.getState());
+        t1.join(); //main thread will wait for t1 to complete its execution
+        System.out.println(t1.getState()); //thread t1 complete its execution, hence terminated state
+
+    }
 }
